42a5aa7e7e0a53b1cf5c679d44b1c4930ea69479
amicabledevil <amicabledevil@gmail.com>
2025-10-19 14:19:32 +0530

    edited map

--- mapping.go
+++ mapping.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"maps"
 )
 
 func mapping() {
@@ -15,4 +16,25 @@ func mapping() {
 	mv1 := m["val1"]
 	mv2 := m["val3"]
 	fmt.Println("do they exist", mv1, mv2)
+
+	fmt.Println("len: ", len(m))
+
+	delete(m, "val2")
+	fmt.Println("updated:", m)
+	clear(m)
+	fmt.Println("empty:", m)
+
+	_, prs := m["val1"] //_ says that ignore the value and focus on the key if it exist so they give either true or false
+	fmt.Println("exist?:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map", n)
+
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n==n2")
+	} else {
+		fmt.Println("n!=n2")
+	}
+
 }
